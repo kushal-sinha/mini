c4714229ac657ee7278015cb7957e81599147e30
kushal-sinha <117979533+kushal-sinha@users.noreply.github.com>
2024-03-06 23:02:37 +0530

    Add files via upload

--- resetpassword.php
+++ resetpassword.php
@@ -2,23 +2,21 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
+error_reporting(0);
 
 if(isset($_POST['submit']))
   {
-    $contactno=$_POST['contactno'];
-    $email=$_POST['email'];
+    $contactno=$_SESSION['contactno'];
+    $email=$_SESSION['email'];
+    $password=md5($_POST['newpassword']);
 
-        $query=mysqli_query($con,"select ID from tbladmin where  Email='$email' and MobileNumber='$contactno' ");
-    $ret=mysqli_fetch_array($query);
-    if($ret>0){
-      $_SESSION['contactno']=$contactno;
-      $_SESSION['email']=$email;
-     
-     echo "<script type='text/javascript'> document.location ='resetpassword.php'; </script>";
-    }
-    else{
-      echo "<script>alert('Invalid Details. Please try again.');</script>";
-    }
+        $query=mysqli_query($con,"update tbladmin set Password='$password'  where  Email='$email' && MobileNumber='$contactno' ");
+   if($query)
+   {
+echo "<script>alert('Password successfully changed');</script>";
+session_destroy();
+   }
+  
   }
   ?>
 
@@ -26,9 +24,22 @@ if(isset($_POST['submit']))
 <html lang="en">
     <head>
        
-        <title>Nursery Management System - Forgot Password</title>
+        <title>Nursery Management System - Reset Password</title>
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+        <script type="text/javascript">
+function checkpass()
+{
+if(document.changepassword.newpassword.value!=document.changepassword.confirmpassword.value)
+{
+alert('New Password and Confirm Password field does not match');
+document.changepassword.confirmpassword.focus();
+return false;
+}
+return true;
+} 
+
+</script>
     </head>
     <body class="bg-primary">
         <div id="layoutAuthentication">
@@ -38,16 +49,16 @@ if(isset($_POST['submit']))
                         <div class="row justify-content-center">
                             <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Forgot Password</h3></div>
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Reset Password</h3></div>
                                     <div class="card-body">
-                                        <form method="post">
+                                        <form method="post" name="changepassword" onsubmit="return checkpass();">
                                             <div class="form-floating mb-3">
-                                                 <input type="email" class="form-control" placeholder="Email" name="email" required="">
-                                                <label for="username">Email</label>
+                                                 <input class="form-control" type="password" required="" name="newpassword" placeholder="New Password">
+                                                <label for="username">New Password</label>
                                             </div>
                                             <div class="form-floating mb-3">
-                                                <input type="text" class="form-control" placeholder="Mobile Number" required="" name="contactno" maxlength="10" pattern="[0-9]{10}">
-                                                <label for="inputPassword">Mobile Number</label>
+                                                <input class="form-control" type="password" name="confirmpassword" required="" placeholder="Confirm Your Password">
+                                                <label for="inputPassword">Confirm Password</label>
                                             </div>
                                             
                                             <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
